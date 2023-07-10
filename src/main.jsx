455dcd1e9a828b009c1fd813fa65022d2c123eef
jorgeabrahan <jorge24abrahan@gmail.com>
2023-07-10 00:55:32 -0600

    Change imports order

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { JournalApp } from './JournalApp'
-import './styles.css'
 import { BrowserRouter } from 'react-router-dom'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
