5b01a1835daba78a65f8ed1206f4253df6d6d70c
jorgeabrahan <jorge24abrahan@gmail.com>
2023-07-15 14:49:25 -0600

    Handle form login logic with and without google, submit and validation

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,37 +1,96 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { Link as RouterLink } from 'react-router-dom'
-import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Alert, Button, Grid, Link, TextField, Typography } from '@mui/material'
 import { Google } from '@mui/icons-material'
 import { AuthLayout } from '../layout'
+import { useForm } from '../../hooks'
+import { googleSignIn, loginUser } from '../../store/auth/'
+import { useMemo, useState } from 'react'
+
+const formData = {
+  email: '',
+  password: ''
+}
+
+const formValidations = {
+  email: [(value) => value.trim().includes('@'), 'El correo debe contener el simbolo @'],
+  password: [(value) => value.trim().length !== 0, 'El campo de contraseña no puede estar vacio']
+}
 
 export const LoginPage = () => {
+  const dispatch = useDispatch()
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const { status, errorMessage } = useSelector((store) => store.auth)
+  const { email, password, onInputChange, emailError, passwordError, isFormValid, formState } =
+    useForm(formData, formValidations)
+  const isAuthenticating = useMemo(() => status === 'checking', [status])
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setFormSubmitted(true)
+    if (!isFormValid) return
+    dispatch(loginUser(formState))
+  }
+  const onGoogleSignIn = () => {
+    dispatch(googleSignIn())
+  }
   return (
     <AuthLayout title="Login">
-      <Grid container>
-        <Grid item xs={12} sx={{ mt: 2 }}>
-          <TextField label="Correo" type="email" placeholder="correo@google.com" fullWidth />
-        </Grid>
-        <Grid item xs={12} sx={{ mt: 2 }}>
-          <TextField label="Contraseña" type="password" placeholder="C0n7ra5en1a" fullWidth />
-        </Grid>
-        <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-          <Grid item xs={12} sm={6}>
-            <Button variant="contained" fullWidth>
-              Login
-            </Button>
+      <form onSubmit={onSubmit} className="animate__animated animate__fadeIn animate__faster">
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
+              error={!!emailError && formSubmitted}
+              helperText={formSubmitted && emailError}
+            />
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Button variant="contained" fullWidth>
-              <Google />
-              <Typography sx={{ ml: 1 }}>Google</Typography>
-            </Button>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="C0n7ra5en1a"
+              fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
+              error={!!passwordError && formSubmitted}
+              helperText={formSubmitted && passwordError}
+            />
+          </Grid>
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} display={errorMessage ? '' : 'none'}>
+              <Alert severity="error">{errorMessage}</Alert>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button disabled={isAuthenticating} type="submit" variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button
+                disabled={isAuthenticating}
+                variant="contained"
+                fullWidth
+                onClick={onGoogleSignIn}
+              >
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/register">
+              Crear una cuenta
+            </Link>
           </Grid>
         </Grid>
-        <Grid container direction="row" justifyContent="end">
-          <Link component={RouterLink} color="inherit" to="/auth/register">
-            Crear una cuenta
-          </Link>
-        </Grid>
-      </Grid>
+      </form>
     </AuthLayout>
   )
 }
