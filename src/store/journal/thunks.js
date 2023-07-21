f9c253a1f31abb4fc19b3e7e2415dac73cd15c4e
jorgeabrahan <jorge24abrahan@gmail.com>
2023-07-20 22:12:07 -0600

    Create thunks to load, save or update and create a new empty note

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,9 +1,46 @@
+import { collection, doc, setDoc } from 'firebase/firestore/lite'
+import { FirebaseFirestore } from '../../firebase/config'
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setSaving, updateNote } from './'
+import { loadNotes } from '../../helpers'
+
 export const startNewNote = () => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    dispatch(savingNewNote())
+
+    const { uid } = getState().auth
     const newNote = {
       title: '',
       body: '',
       date: new Date().getTime()
     }
+    const newDoc = doc(collection(FirebaseFirestore, `${uid}/journal/notes`))
+    await setDoc(newDoc, newNote)
+    newNote.id = newDoc.id
+    dispatch(addNewEmptyNote(newNote))
+    dispatch(setActiveNote(newNote))
+  }
+}
+
+export const startLoadingNotes = () => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth
+    if (!uid) throw new Error('El uid del usuario no existe')
+    const notes = await loadNotes(uid)
+    dispatch(setNotes(notes))
+  }
+}
+
+export const startSavingNote = () => {
+  return async (dispatch, getState) => {
+    dispatch(setSaving())
+    const { uid } = getState().auth
+    const { active: note } = getState().journal
+
+    const noteToFireStore = { ...note }
+    delete noteToFireStore.id
+
+    const docRef = doc(FirebaseFirestore, `${uid}/journal/notes/${note.id}`)
+    await setDoc(docRef, noteToFireStore, { merge: true })
+    dispatch(updateNote(note))
   }
 }
