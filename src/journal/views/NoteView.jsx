7ccbfd277d9904180a08b9bf29760ba1b7f949e9
jorgeabrahan <jorge24abrahan@gmail.com>
2023-07-20 22:17:53 -0600

    Add logic to display selected note and to handle the edit form within the view

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,8 +1,26 @@
 import { SaveOutlined } from '@mui/icons-material'
 import { Button, Grid, TextField, Typography } from '@mui/material'
 import { ImageGallery } from '../components'
+import { useDispatch, useSelector } from 'react-redux'
+import { useForm } from '../../hooks/'
+import { useEffect, useMemo } from 'react'
+import { setActiveNote, startSavingNote } from '../../store/journal'
 
 export const NoteView = () => {
+  const dispatch = useDispatch()
+  const { active: note } = useSelector((store) => store.journal)
+  const { body, title, date, onInputChange, formState } = useForm(note)
+  const dateString = useMemo(() => {
+    const newDate = new Date(date)
+    return newDate.toUTCString()
+  }, [date])
+  useEffect(() => {
+    dispatch(setActiveNote(formState))
+  }, [formState])
+
+  const onSaveNote = () => {
+    dispatch(startSavingNote())
+  }
   return (
     <Grid
       className="animate__animated animate__fadeIn animate__faster"
@@ -14,11 +32,11 @@ export const NoteView = () => {
     >
       <Grid item>
         <Typography fontSize={39} fontWeight="light">
-          28 de agosto, 2023
+          {dateString}
         </Typography>
       </Grid>
       <Grid item>
-        <Button color="primary" sx={{ padding: 2 }}>
+        <Button color="primary" sx={{ padding: 2 }} onClick={onSaveNote}>
           <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
           Guardar
         </Button>
@@ -31,6 +49,9 @@ export const NoteView = () => {
           placeholder="Ingrese un título"
           label="Título"
           sx={{ border: 'none', mb: 1 }}
+          name="title"
+          value={title}
+          onChange={onInputChange}
         />
         <TextField
           type="text"
@@ -40,6 +61,9 @@ export const NoteView = () => {
           placeholder="¿Qué sucedio en el día de hoy?"
           label="Descripción"
           minRows={5}
+          name="body"
+          value={body}
+          onChange={onInputChange}
         />
       </Grid>
       <ImageGallery />
