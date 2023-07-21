7940db974da0f1588a4569b7bec78c75146ca1aa
jorgeabrahan <jorge24abrahan@gmail.com>
2023-07-20 22:17:00 -0600

    Add condition to decide what to render depending on if there's an active note or not and handle click event on the button to add a new note

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { IconButton } from '@mui/material'
 import { JournalLayout } from '../layout/'
 import { NoteView, NothingSelectedView } from '../views'
 import { AddOutlined } from '@mui/icons-material'
+import { startNewNote } from '../../store/journal'
 
 export const JournalPage = () => {
+  const dispatch = useDispatch()
+  const { isSaving, active } = useSelector((store) => store.journal)
+  const onClickNewNote = () => {
+    dispatch(startNewNote())
+  }
+
   return (
     <JournalLayout>
-      <NothingSelectedView />
-      {/* <NoteView /> */}
+      {active !== null ? <NoteView /> : <NothingSelectedView />}
+
       <IconButton
+        onClick={onClickNewNote}
+        disabled={isSaving}
         size="large"
         sx={{
           color: 'white',
