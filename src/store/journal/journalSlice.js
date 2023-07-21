7277bbae39ec274ef41bb1f18e4ff4984a2825bd
jorgeabrahan <jorge24abrahan@gmail.com>
2023-07-21 07:29:17 -0600

    Change message to display on popup when a note is updated

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -21,12 +21,14 @@ export const journalSlice = createSlice({
     },
     setActiveNote: (state, action) => {
       state.active = action.payload
+      state.messageSaved = ''
     },
     setNotes: (state, action) => {
       state.notes = action.payload
     },
     setSaving: (state) => {
       state.isSaving = true
+      state.messageSaved = ''
     },
     updateNote: (state, action) => {
       state.isSaving = false
@@ -34,6 +36,7 @@ export const journalSlice = createSlice({
         if (note.id !== action.payload.id) return note
         return action.payload
       })
+      state.messageSaved = 'Nota actualizada correctamente'
     },
     deleteNoteById: (state, action) => {}
   }
